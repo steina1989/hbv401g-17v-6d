eb356f05d15cc86b96bfb5d378989d2617d8a493
steina1989 <steina.dogg@gmail.com>
2017-03-21 17:26:02 +0000

    Modified Trip

--- model/Trip.java
+++ model/Trip.java
@@ -13,6 +13,70 @@ public class Trip {
 	private String description;
 	private int seatsAvailable;
 	private String category;
-	private ArrayList<Guide> guides;	
+	private ArrayList<Guide> guides;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public ArrayList<Review> getReviews() {
+		return reviews;
+	}
+	public void setReviews(ArrayList<Review> reviews) {
+		this.reviews = reviews;
+	}
+	public Location getLocation() {
+		return location;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getSeatsAvailable() {
+		return seatsAvailable;
+	}
+	public void setSeatsAvailable(int seatsAvailable) {
+		this.seatsAvailable = seatsAvailable;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public ArrayList<Guide> getGuides() {
+		return guides;
+	}
+	public void setGuides(ArrayList<Guide> guides) {
+		this.guides = guides;
+	}	
 
+	
+	
 }
