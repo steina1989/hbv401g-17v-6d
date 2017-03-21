2664bfb3d90d94245d337d0a81c2132d24e4af3c
steina1989 <steina.dogg@gmail.com>
2017-03-21 17:48:27 +0000

    Edited ListOfTripsPanel

--- view/ListOfTripsPanel.java
+++ view/ListOfTripsPanel.java
@@ -1,6 +1,7 @@
 package view;
 
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class ListOfTripsPanel extends JPanel {
 
@@ -8,6 +9,10 @@ public class ListOfTripsPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public ListOfTripsPanel() {
+		
+		JTextArea txtrBla = new JTextArea();
+		txtrBla.setText("bla");
+		add(txtrBla);
 
 	}
 
