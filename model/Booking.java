39efecb732b87eb0c228dcf92a70f8bae4c59cb5
steina1989 <steina.dogg@gmail.com>
2017-03-21 16:52:19 +0000

    Added Booking model

--- model/Booking.java
+++ model/Booking.java
@@ -2,7 +2,53 @@ package model;
 
 public class Booking {
 
-	Buyer buyer;
+	private Buyer buyer;
+	private int bookingId;
+	private Trip trip;
+	private int numberOfGuests;
+	private Date timeOfBooking;
+	private String comment;
+	
+	
+	public Buyer getBuyer() {
+		return buyer;
+	}
+	public void setBuyer(Buyer buyer) {
+		this.buyer = buyer;
+	}
+	public int getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+	public Trip getTrip() {
+		return trip;
+	}
+	public void setTrip(Trip trip) {
+		this.trip = trip;
+	}
+	public int getNumberOfGuests() {
+		return numberOfGuests;
+	}
+	public void setNumberOfGuests(int numberOfGuests) {
+		this.numberOfGuests = numberOfGuests;
+	}
+	public Date getTimeOfBooking() {
+		return timeOfBooking;
+	}
+	public void setTimeOfBooking(Date timeOfBooking) {
+		this.timeOfBooking = timeOfBooking;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	
 	
 
 }
