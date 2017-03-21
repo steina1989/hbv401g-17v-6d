31cb4b35e1880e635b90f39959983714ec3595b7
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-03-21 17:08:12 +0000

    Add Guide class

--- model/Guide.java
+++ model/Guide.java
@@ -1,5 +1,49 @@
 package model;
 
-public class Guide {
+import java.awt.image.BufferedImage;
 
+public class Guide {
+	private String name;
+	private int age;
+	private String gender;
+	private String email;
+	private String cellphone;
+	private BufferedImage image;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCellphone() {
+		return cellphone;
+	}
+	public void setCellphone(String cellphone) {
+		this.cellphone = cellphone;
+	}
+	public BufferedImage getImage() {
+		return image;
+	}
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
 }
