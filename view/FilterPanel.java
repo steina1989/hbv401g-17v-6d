d6c46e6bf7933df548e018168366c4ec0ea7d62a
steina1989 <steina.dogg@gmail.com>
2017-03-21 18:16:33 +0000

    Created FilterPanel.java

---
+++ view/FilterPanel.java
@@ -0,0 +1,34 @@
+package view;
+
+import javax.swing.JPanel;
+import java.util.Date;
+import javax.swing.JTextArea;
+import javax.swing.JList;
+import javax.swing.JCheckBox;
+import javax.swing.JSlider;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JSplitPane;
+
+public class FilterPanel extends JPanel {
+
+	private Date dateLow;
+	private Date dateHigh;
+	private int priceLow;
+	private int priceHigh;
+	
+	/**
+	 * Create the panel.
+	 */
+	public FilterPanel() {
+
+	}
+	
+	
+	
+	
+	
+
+}
