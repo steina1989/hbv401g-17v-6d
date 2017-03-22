1649169ce0ca32b9e04cb9c56a46e3cc249b0ae5
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-03-22 18:05:28 +0000

    Created ViewBookingsPanel.java file

---
+++ view/ViewBookingsPanel.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class ViewBookingsPanel extends JPanel {
+	private BookingController bookingController;
+	private JButton cancelBooking;
+	
+	/**
+	 * Create the panel.
+	 */
+	public ViewBookingsPanel() {
+		
+	}
+
+}
