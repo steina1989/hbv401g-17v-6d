1910eca3c6713327c56aec941bfccf6ceba3187a
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-03-29 16:53:12 +0000

    Basic Implementation of ListOfTripsPanel

--- view/ListOfTripsPanel.java
+++ view/ListOfTripsPanel.java
@@ -2,15 +2,34 @@ package view;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 
 public class ListOfTripsPanel extends JPanel {
+	
+	private JList tripList;
+	private DefaultListModel<String> tripNames;
 
 	/**
 	 * Create the panel.
 	 */
 	public ListOfTripsPanel() {
-		JTextArea txtrBla = new JTextArea();
-		txtrBla.setText("bla");
-		add(txtrBla);
+		setLayout(null);
+		
+		tripNames = new DefaultListModel<String>();
+		this.addExampleTripNames();
+		
+		JList list = new JList();
+		list.setBounds(0, 0, 209, 300);
+		list.setModel(tripNames);
+		add(list);
+	}
+	
+	private void addExampleTripNames() {
+		this.tripNames.addElement("TripName1");
+		this.tripNames.addElement("TripName2");
+		this.tripNames.addElement("TripName3");
+		this.tripNames.addElement("TripName4");
+		this.tripNames.addElement("TripName5");
 	}
 }
